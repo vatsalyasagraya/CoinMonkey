bba2d257294b6c5f8c310e4c00ced4f368280ae5
vatsalyasagraya <2020uec1198@mnit.ac.in>
2022-08-12 15:06:56 +0530

    first commit

---
+++ src/components/Coin.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./coin.css";
+
+function Coin({ name, icon, price, symbol,pricechange }) {
+  return (
+    
+    <div className="coin">
+      <img src={icon} alt="coin" className="coinImage" />
+      <div className="name">{name}</div> 
+      <div className="symbol">{symbol}</div> 
+      <div className={`pricechange ${pricechange<0?'red':'green'}` }>{pricechange}</div>
+      <div className="price">${price}</div>
+    </div>
+  );
+}
+
+export default Coin;
